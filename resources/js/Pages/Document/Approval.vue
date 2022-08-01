c8f2d8953ebaf3e7a59c14619b5f6459d18ad297
Geriano <gerznewbie@gmail.com>
2022-08-01 10:23:56 +0700

    refactor document approval

--- resources/js/Pages/Document/Approval.vue
+++ resources/js/Pages/Document/Approval.vue
@@ -40,9 +40,9 @@ onUnmounted(() => window.removeEventListener('keydown', esc))
 
 <template>
   <DashboardLayout :title="__('approval')">
-    <Card class="flex flex-col bg-white dark:bg-gray-700 dark:text-gray-200 rounded-md">
+    <Card class="bg-white dark:bg-gray-700 dark:text-gray-200">
       <template #header>
-        <div class="flex items-center space-x-2 bg-slate-200 dark:bg-gray-800 rounded-t-md p-2">
+        <div class="flex items-center space-x-2 bg-gray-200 dark:bg-gray-800 rounded-t-md p-2">
           <Link :href="route('document.index')">
             <ButtonDark class="bg-gray-700">
               <Icon name="caret-left" />
@@ -58,32 +58,32 @@ onUnmounted(() => window.removeEventListener('keydown', esc))
       </template>
 
       <template #body>
-        <div class="flex flex-col space-y-4 p-4">
-          <div class="rounded-md overflow-auto border border-slate-300 dark:border-gray-900">
+        <div class="p-4">
+          <div class="rounded-md overflow-auto border border-gray-300 dark:border-gray-900">
             <table class="w-full border-collapse">
-              <thead class="bg-slate-200 dark:bg-gray-800">
-                <tr>
-                  <th class="border border-slate-300 dark:border-gray-900 py-2 text-center uppercase" rowspan="2">{{ __('no') }}</th>
-                  <th v-for="(approver, i) in approvers" :key="i" class="border border-slate-300 dark:border-gray-900 text-center font-semibold uppercase px-2" colspan="3">
+              <thead class="bg-gray-200 dark:bg-gray-800 border-gray-300 dark:border-gray-900">
+                <tr class="bg-inherit border-inherit">
+                  <th class="border border-inherit py-2 text-center uppercase" rowspan="2">{{ __('no') }}</th>
+                  <th v-for="(approver, i) in approvers" :key="i" class="border border-inherit text-center font-semibold uppercase px-3" colspan="3">
                     {{ approver.user.name }}
                   </th>
                 </tr>
 
-                <tr>
+                <tr class="bg-inherit border-inherit">
                   <template v-for="(approver, i) in approvers" :key="i">
-                    <th class="border border-slate-300 dark:border-gray-900 text-center font-semibold uppercase px-2">{{ __('status') }}</th>
-                    <th class="border border-slate-300 dark:border-gray-900 text-center font-semibold uppercase px-2">{{ __('responded at') }}</th>
-                    <th class="border border-slate-300 dark:border-gray-900 text-center font-semibold uppercase px-2">{{ __('note') }}</th>
+                    <th class="border border-inherit text-center font-semibold uppercase px-2">{{ __('status') }}</th>
+                    <th class="border border-inherit text-center font-semibold uppercase px-2">{{ __('responded at') }}</th>
+                    <th class="border border-inherit text-center font-semibold uppercase px-2">{{ __('note') }}</th>
                   </template>
                 </tr>
               </thead>
 
               <tbody>
-                <tr v-for="(approve, i) in approves" :key="i" class="hover:bg-slate-100 dark:hover:bg-gray-800 transition-all">
-                  <td class="border border-slate-300 dark:border-gray-900 text-center p-1">{{ i + 1 }}</td>
+                <tr v-for="(approve, i) in approves" :key="i" class="hover:bg-gray-100 dark:hover:bg-gray-600 border-gray-300 dark:border-gray-800 transition-all">
+                  <td class="border border-inherit text-center p-1">{{ i + 1 }}</td>
                   
                   <template v-for="(approval, j) in approve.approvals" :key="j">
-                    <td class="border border-slate-300 dark:border-gray-900 text-center px-1">
+                    <td class="border border-inherit text-center px-1">
                       <Button
                         :class="{
                           'bg-orange-600 hover:bg-orange-700': approval.status === 'pending',
@@ -95,11 +95,11 @@ onUnmounted(() => window.removeEventListener('keydown', esc))
                       </Button>
                     </td>
 
-                    <td class="border border-slate-300 dark:border-gray-900 text-center px-1">
+                    <td class="border border-inherit text-center px-1">
                       {{ responded(approval) || '' }}
                     </td>
 
-                    <td class="border border-slate-300 dark:border-gray-900 text-center px-1">
+                    <td class="border border-inherit text-center px-1">
                       <ButtonBlue v-if="approval.responder_note" @click.prevent="note = approval.responder_note">
                         <Icon name="newspaper" />
                         <p class="uppercase font-semibold">{{ __('note') }}</p>
@@ -116,15 +116,15 @@ onUnmounted(() => window.removeEventListener('keydown', esc))
   </DashboardLayout>
 
   <Modal :show="note !== ''">
-    <Card class="flex flex-col bg-white dark:bg-gray-700 dark:text-gray-200 w-full max-w-xl rounded-md shadow-xl">
+    <Card class="bg-white dark:bg-gray-700 dark:text-gray-200 w-full max-w-xl shadow-xl">
       <template #header>
-        <div class="flex items-center justify-end space-x-2 bg-slate-200 dark:bg-gray-800 rounded-t-md p-2">
+        <div class="flex items-center justify-end space-x-2 bg-gray-200 dark:bg-gray-800 p-2">
           <Close @click.prevent="note = ''" />
         </div>
       </template>
 
       <template #body>
-        <div class="flex flex-col space-y-2 p-4 rounded-b-md">
+        <div class="p-4 max-h-96 overflow-auto">
           <p>{{ note }}</p>
         </div>
       </template>
